cd08cd9b7bcd6bdc69f4415f3b6687b32733e988
Senseye76 <kinojoker12@gmail.com>
2026-09-03 14:46:51 +0200

    Feature/LoginForm done

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const loginData = {
+      username: username,
+      password: password,
+    };
+
+    //till backend
+  // const response = await fetch("http://localhost:3000/api/login", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(loginData),
+  // });
+
+  //------------------------------------------------------
+    //tabort console.log när du skickar till backend
+
+    console.log("username:", loginData.username);
+    console.log("password:", "*".repeat(loginData.password.length));
+
+
+    navigate("/rules");
+  }
+
+
   return (
     <div className=" mt-10">
-      <form className="flex items-center justify-center flex-col gap-4 ">
+      <form
+        className="flex items-center justify-center flex-col gap-4 "
+        onSubmit={handleSubmit}
+      >
         <div className="items-center justify-center flex-col">
           <label className="block" htmlFor="username">
             Användarnamn
@@ -10,8 +48,11 @@ function LoginForm() {
             type="text"
             id="username"
             name="username"
-            placeholder="Skriv ditt användernamn"
-            alt="Användarnamn"
+            placeholder="Ange användarnamn"
+            value={username}
+            autoComplete="username"
+            required
+            onChange={(e) => setUsername(e.target.value)}
             className=" text-center border border-gray-300 rounded-md p-2 px-10  focus:placeholder-transparent"
           />
         </div>
@@ -25,7 +66,10 @@ function LoginForm() {
             id="password"
             name="password"
             placeholder="********"
-            alt="Lösenord"
+            value={password}
+            autoComplete="current-password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
             className=" text-center border border-gray-300 rounded-md p-2 px-10  focus:placeholder-transparent"
           />
         </div>
